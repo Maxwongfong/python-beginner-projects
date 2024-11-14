69c980b0ff5cd2b3839b9402edf100531b88fc1d
Maxwongfong <2944531@gmail.com>
2024-11-14 17:04:53 +0800

    Update advisor.py

--- projects/Advisor/advisor.py
+++ projects/Advisor/advisor.py
@@ -1,4 +1,4 @@
-# Importing libraries
+import time
 from tkinter.constants import DISABLED
 import random
 import requests
@@ -27,8 +27,6 @@ def back_to_previous():
     global previous_advice
 
     if previous_advice:
-        #current_advice = advice_text.get()
-        #advice_text.set(previous_advice)
         previous_advice = previous_advice_set[random.randint(0,len(previous_advice_set))-1]
         advice_text.set(previous_advice)
     if not previous_advice:
@@ -37,6 +35,7 @@ def back_to_previous():
 def clear_button_advice():
     previous_advice_set.clear()
     return advice()
+
 # Create the main window
 root = tk.Tk()
 root.title("Random Advisor Application")
@@ -45,18 +44,28 @@ root.title("Random Advisor Application")
 advice_text = tk.StringVar()
 previous_advice = ""
 advice_label = tk.Label(
-    root, textvariable=advice_text, wraplength=400, font=("Arial", 14)
+    root, textvariable=advice_text, wraplength=400, font=("Arial", 18)
 )
+time_label = tk.Label(root,fg='red',font=("Arial", 24))
 get_advice_button = tk.Button(root, text="Get Advice", command=advice)
 get_back_button = tk.Button(root,text="Previous advice",command=back_to_previous,state=DISABLED)
 clear_button = tk.Button(root,text="Restart",command = clear_button_advice,state =DISABLED )
 # Pack widgets
+time_label.pack(side = 'top',pady =10)
 advice_label.pack(side = 'top',pady= 40)
-get_advice_button.pack(side = 'left',pady =10)
 get_back_button.pack(side = 'left',pady =10)
 clear_button.pack(side= 'right',pady =10)
+get_advice_button.pack(side = 'right',pady =10)
+
+
+def update_time():
+    current_time = time.strftime('%H:%M:%S')
+    time_label.config(text=current_time)
+    root.after(1000, update_time)
+
 # Initial advice fetching
 advice()
+update_time()
 
 # Run the main event loop
 root.mainloop()
