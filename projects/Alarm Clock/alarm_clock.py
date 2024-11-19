ad56ef2a08fa9e749c54de3dc3cddf61912eb1ae
Maxwongfong <2944531@gmail.com>
2024-11-19 10:45:30 +0800

    Update alarm_clock.py

--- projects/Alarm Clock/alarm_clock.py
+++ projects/Alarm Clock/alarm_clock.py
@@ -2,18 +2,13 @@ import tkinter as tk
 import datetime
 import time
 from threading import Thread
-import pygame
-from pygame import mixer
 
+#from nltk.sem.chat80 import continent
+from pygame import mixer
 
 # create object
 root = tk.Tk()
 root.geometry("500x250")
-
-
-#def threading():
-#    t1 = Thread(target=alarm)
-#    t1.start()
 
 alarm_setting = False
 current_thread = None
@@ -21,13 +16,14 @@ current_thread = None
 def alarm():
     # alarm set to an infinite loop
     global alarm_setting
+    front_time()
     while alarm_setting:
         # alarm set
         set_alarm_time = f"{hour.get()}:{minute.get()}:{second.get()}"
         time.sleep(1)
         # get current time
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        print(current_time, set_alarm_time)
+        print(f"当前时间：{current_time}",f"闹钟时间：{set_alarm_time}")
 
         # condition to check if set time is equal to current time
         if current_time == set_alarm_time:
@@ -35,43 +31,41 @@ def alarm():
             # play sound continuously
             mixer.init()
             mixer.music.load("sound.wav")
-            mixer.music.play(loops=3)
+            mixer.music.play(loops=5)
 
 
 def stop_alarm():
-    global alarm_setting
-    global current_thread
-    try:
-        if mixer.get_init():
-            alarm_setting = False
-            mixer.music.stop()
-        elif current_thread is not None:
-            current_thread.join()
-            current_thread = None
-    except pygame.error:
-        print("闹钟尚未启动，无需停止！")
-        #alarm_setting = False
+    #global alarm_setting
+    #global current_thread
+    if mixer.get_init():
+        mixer.music.stop()
+    elif current_thread is not None:
+        print("未到闹钟时间，无需停止！")
 
-    #if current_thread is not None:
-        #current_thread.join()
-        #current_thread = None
 
 def threading():
     global alarm_setting
     global current_thread
     if current_thread is not None:
-        stop_alarm()
+        pass
+        #stop_alarm()
+    else:
+        alarm_setting = True
+        current_thread = Thread(target=alarm)
+        current_thread.start()
 
-    alarm_setting = True
-    current_thread = Thread(target=alarm)
-    current_thread.start()
-
-
+def front_time():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    time_clock.config(text = current_time)
+    root.after(1000,front_time)
 
 
 
 tk.Label(root, text="Alarm Clock", font="Helvetica 20 bold", fg="red").pack(pady=10)
 tk.Label(root, text="Set Time", font="Helvetica 15 bold").pack()
+time_clock= tk.Label(root,fg="red",font=("Arial", 24))
+time_clock.pack(ipady=25, side=tk.TOP)
+
 
 frame = tk.Frame(root)
 frame.pack()
@@ -81,21 +75,18 @@ hours = [f"{i:02}" for i in range(24)]
 hour.set(hours[0])
 
 tk.OptionMenu(frame, hour, *hours).pack(side = tk.LEFT)
-#hrs.pack(side = tk.LEFT)
 
 minute = tk.StringVar(root)
 minutes = [f"{i:02}" for i in range(60)]
 minute.set(minutes[0])
 
 tk.OptionMenu(frame, minute, *minutes).pack(side = tk.LEFT)
-#mins.pack(side = tk.LEFT)
 
 second = tk.StringVar(root)
 seconds = [f"{i:02}" for i in range(60)]
 second.set(seconds[0])
 
 tk.OptionMenu(frame, second, *seconds).pack(side = tk.LEFT)
-#secs.pack(side = tk.LEFT)
 
 tk.Button(root, text="Set Alarm", font="Helvetica 15", command=threading).pack(pady=20)
 
